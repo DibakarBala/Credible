e434ce7fafdd17aae2d10bf684f47d7c804e17e4
Dibakar Bala <baladibakar89@gmail.com>
2024-10-05 20:30:35 +0530

    ui updates

--- target/release/deps/unicode_ident-7fa9ec413f5f073c.d
+++
@@ -1,8 +0,0 @@
-\\?\D:\Solana\target\release\deps\libunicode_ident-7fa9ec413f5f073c.rmeta: C:\Users\Dibakar\.cargo\registry\src\index.crates.io-6f17d22bba15001f\unicode-ident-1.0.12\src\lib.rs C:\Users\Dibakar\.cargo\registry\src\index.crates.io-6f17d22bba15001f\unicode-ident-1.0.12\src\tables.rs
-
-\\?\D:\Solana\target\release\deps\libunicode_ident-7fa9ec413f5f073c.rlib: C:\Users\Dibakar\.cargo\registry\src\index.crates.io-6f17d22bba15001f\unicode-ident-1.0.12\src\lib.rs C:\Users\Dibakar\.cargo\registry\src\index.crates.io-6f17d22bba15001f\unicode-ident-1.0.12\src\tables.rs
-
-\\?\D:\Solana\target\release\deps\unicode_ident-7fa9ec413f5f073c.d: C:\Users\Dibakar\.cargo\registry\src\index.crates.io-6f17d22bba15001f\unicode-ident-1.0.12\src\lib.rs C:\Users\Dibakar\.cargo\registry\src\index.crates.io-6f17d22bba15001f\unicode-ident-1.0.12\src\tables.rs
-
-C:\Users\Dibakar\.cargo\registry\src\index.crates.io-6f17d22bba15001f\unicode-ident-1.0.12\src\lib.rs:
-C:\Users\Dibakar\.cargo\registry\src\index.crates.io-6f17d22bba15001f\unicode-ident-1.0.12\src\tables.rs:
