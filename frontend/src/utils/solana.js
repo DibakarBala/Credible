b49db831c3216f753f51881bcbb01a7af33c8653
Dibakar Bala <baladibakar89@gmail.com>
2024-10-07 01:58:27 +0530

    commented out const fs and const path from solana.js

--- frontend/src/utils/solana.js
+++ frontend/src/utils/solana.js
@@ -1,6 +1,6 @@
 const { Connection, PublicKey, Keypair, Transaction, SystemProgram } = require('@solana/web3.js');
-const fs = require('fs');
-const path = require('path');
+//const fs = require('fs');
+//const path = require('path');
 
 const SOLANA_NETWORK = 'https://api.devnet.solana.com'; // Use the appropriate network
 const WALLET_ADDRESS = new PublicKey('BCkWwZyaNZQCwmVPu7C75TZkyDBjoX9m5NwYZcUTmBHe'); // Replace with your actual wallet address
